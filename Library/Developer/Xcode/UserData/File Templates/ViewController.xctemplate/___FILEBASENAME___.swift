cb72eacd7649ca26c91fffb1380fb1bd5e72fa77
ohlulu <z30262226@gmail.com>
2021-09-21 17:43:26 +0800

    Add Xcode UserData setting

---
+++ Library/Developer/Xcode/UserData/File Templates/ViewController.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,52 @@
+//___FILEHEADER___
+
+import UIKit
+
+final class ___FILEBASENAME___: UIViewController {
+
+    // UI elements
+
+    // Properties
+
+    // Life cycle
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - ViewModel delegate
+
+extension ___FILEBASENAME___ {
+
+}
+
+// MARK: - Target action
+
+private extension ___FILEBASENAME___ {
+
+}
+
+// MARK: - Helper
+
+private extension ___FILEBASENAME___ {
+
+}
+
+// MARK: - UI methods
+
+private extension ___FILEBASENAME___ {
+
+    func setupUI() {
+        view.backgroundColor = .white
+    }
+}
