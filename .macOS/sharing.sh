749122dd0baa5f801b8b815c0cf291e14acf806f
ohlulu <z30262226@gmail.com>
2022-09-23 10:13:21 +0800

    Update .macOS

--- .macOS/sharing.sh
+++
@@ -1,7 +0,0 @@
-# Set computer name ohluluㄉ賣不可 (as done via System Preferences → Sharing)
-# https://coding.tools/tw/ascii-to-hex
-
-# sudo scutil --set ComputerName "0x6F686C756C75"
-# sudo scutil --set HostName "o0x6F686C756C75"
-# sudo scutil --set LocalHostName "0x6F686C756C75"
-# sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "0x6F686C756C75"
