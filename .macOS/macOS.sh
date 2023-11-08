dff044b3c68a0b88570d730f76fe973227d5778b
Ohlulu Hsih <z30262226@gmail.com>
2023-11-07 21:28:39 -0800

    update macOS.sh

--- .macOS/macOS.sh
+++ .macOS/macOS.sh
@@ -81,10 +81,10 @@ defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 # Set computer name ohluluㄉ賣不可 (as done via System Preferences → Sharing)
 # https://coding.tools/tw/ascii-to-hex
 
-sudo scutil --set ComputerName "0x6F686C756C75"
-sudo scutil --set HostName "o0x6F686C756C75"
-sudo scutil --set LocalHostName "0x6F686C756C75"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "0x6F686C756C75"
+# sudo scutil --set ComputerName "0x6F686C756C75"
+# sudo scutil --set HostName "o0x6F686C756C75"
+# sudo scutil --set LocalHostName "0x6F686C756C75"
+# sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "0x6F686C756C75"
 
 ################################################################################
 # screen
